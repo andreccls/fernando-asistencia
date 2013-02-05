a686a2f2865c6147fe9a0f19619b0d3d228c653e
matias.delatorre89@gmail.com <matias.delatorre89@gmail.com@7a8551e6-83ce-ac29-bd34-79ecab084a10>
2013-02-05 21:48:16 +0000

    Vuelto a Iniciar con Reportes

---
+++ trunk/src/TareasProgramadas/ControladorTarea.java
@@ -0,0 +1,190 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TareasProgramadas;
+
+import Clases.Agenda;
+import Clases.Ano;
+import Clases.Asistencia;
+import Clases.Dia;
+import Clases.Iniciofin;
+import Clases.Mes;
+import Clases.Personal;
+import Persistencia.persistencia;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fer
+ */
+public class ControladorTarea {
+    public static persistencia PERSISTENCIA;
+
+    public ControladorTarea() {
+        PERSISTENCIA = new persistencia();
+    }
+
+    public static persistencia getPERSISTENCIA() {
+        return PERSISTENCIA;
+    }
+    
+    public boolean existeAsistencia(Iniciofin infi){
+        Boolean band=false;
+        Iterator it=infi.getAsistencias().iterator();
+        while(it.hasNext()){
+            band=true;
+            it.next();
+        }
+        return band;
+    }
+    
+    public boolean ExisteTareaContinua(Iniciofin in){
+        boolean i=false;
+        Calendar cel=Calendar.getInstance();
+        Personal per=in.getDia().getMes().getAno().getAgenda().getPersonal();
+        Iterator<Iniciofin> it= ControladorTarea.getPERSISTENCIA().ObtenerInicioFin(6, cel.getTime().getMonth(),cel.getTime().getYear()+1900).iterator();
+        cel.setTime(in.getFin());
+        cel.add(Calendar.MINUTE, 30);
+        while(it.hasNext()){
+            Iniciofin ini=it.next();
+            Personal per1=ini.getDia().getMes().getAno().getAgenda().getPersonal();
+            if(cel.getTime().compareTo(ini.getInicio())<=0 && per1.getIdPersonal()==per.getIdPersonal()){
+                    i=true;
+            }
+        }
+        return i;
+    }
+    
+    public void ObtenerListadeldia(){
+        try{
+            Date cal=Calendar.getInstance().getTime();
+            Calendar cel=Calendar.getInstance();
+            Iterator<Iniciofin> it= ControladorTarea.getPERSISTENCIA().ObtenerInicioFin(cel.getTime().getDate(), cel.getTime().getMonth(),cel.getTime().getYear()+1900).iterator();
+            while(it.hasNext()){
+                Iniciofin in= it.next();
+                if(!existeAsistencia(in)){
+                    if(in.getDia().getMes().getAno().getAgenda().getTarea().getEstado()==true && in.getDia().getMes().getAno().getAgenda().getPersonal().getEstado()==true){
+                        if(in.getEstadoInicio()!=null && in.getEstadoFin()!=null){
+                            System.out.println("existe obligación de inicio y fin(clase)");
+                            if(!ExisteTareaContinua(in)){    //no existe tarea continua
+                                if(in.getEstadoInicio()==true && in.getEstadoFin()==true){
+                                    System.out.println("estadoinicio=true y estadofin=true");
+                                    Asistencia asis= new Asistencia();
+                                    asis.setIniciofin(in);
+                                    asis.setEstado(true);
+                                    asis.setTardanza(false);                               
+                                    asis.guardarAsistencia(asis);
+                                }else if(in.getEstadoInicio()==true && in.getEstadoFin()==false){
+                                    System.out.println("ausencia momentanea de uno de los estados");
+                                    cel.setTime(in.getFin());
+                                    cel.add(Calendar.MINUTE, 30);
+                                    String s=new SimpleDateFormat("HH:mm").format(cal);
+                                    SimpleDateFormat formateador=new SimpleDateFormat("HH:mm");
+                                    Date a=formateador.parse(s);
+                                    if(cel.getTime().compareTo(a)<0){
+                                        Asistencia asis= new Asistencia();
+                                        asis.setIniciofin(in);
+                                        asis.setEstado(true);
+                                        asis.setTardanza(true);
+                                        if(!this.existeAsistencia(in)){
+                                        asis.guardarAsistencia(asis);
+                                        }
+                                        System.out.println("tardanza del personal");
+                                    }
+                                }else if(in.getEstadoInicio()==false && in.getEstadoFin()==true){
+                                    System.out.println("ausencia momentanea de uno de los estados");
+                                    cel.setTime(in.getFin());
+                                    cel.add(Calendar.MINUTE, 30);
+                                    String s=new SimpleDateFormat("HH:mm").format(cal);
+                                    SimpleDateFormat formateador=new SimpleDateFormat("HH:mm");
+                                    Date a=formateador.parse(s);
+                                    if(cel.getTime().compareTo(a)<0){
+                                        Asistencia asis= new Asistencia();
+                                        asis.setIniciofin(in);
+                                        asis.setEstado(true);
+                                        asis.setTardanza(true);
+                                        if(!this.existeAsistencia(in)){
+                                        asis.guardarAsistencia(asis);
+                                        }
+                                        System.out.println("tardanza del personal");
+                                    }
+                                }else if(in.getEstadoInicio()==false && in.getEstadoFin()==false){
+                                    System.out.println("ausencia momentanea de uno de los estados");
+                                    cel.setTime(in.getFin());
+                                    cel.add(Calendar.MINUTE, 30);
+                                    String s=new SimpleDateFormat("HH:mm").format(cal);
+                                    SimpleDateFormat formateador=new SimpleDateFormat("HH:mm");
+                                    Date a=formateador.parse(s);
+                                    if(cel.getTime().compareTo(a)<0){
+                                        Asistencia asis= new Asistencia();
+                                        asis.setIniciofin(in);
+                                        asis.setEstado(false);
+                                        asis.setTardanza(false);
+                                        if(!this.existeAsistencia(in)){
+                                        asis.guardarAsistencia(asis);
+                                        }
+                                        System.out.println("ausencia del personal");
+                                    }
+                                }
+                            }else{ //existe tarea continua
+                                if(in.getEstadoInicio()==true && in.getEstadoFin()==true){
+                                    System.out.println("estadoinicio=true y estadofin=true");
+                                    Asistencia asis= new Asistencia();
+                                    asis.setIniciofin(in);
+                                    asis.setEstado(true);
+                                    asis.setTardanza(false);
+                                    if(!this.existeAsistencia(in)){
+                                        asis.guardarAsistencia(asis);
+                                    }
+                                }else if(in.getEstadoInicio()==false && in.getEstadoFin()==true){
+                                    System.out.println("estadoinicio=false y estadofin=true");
+                                    Asistencia asis= new Asistencia();
+                                    asis.setIniciofin(in);
+                                    asis.setEstado(true);
+                                    asis.setTardanza(false);
+                                    if(!this.existeAsistencia(in)){
+                                        asis.guardarAsistencia(asis);
+                                    }                            }
+                            }
+                        }else if(in.getEstadoInicio()!=null && in.getEstadoFin()==null){
+                            System.out.println("existe obligación de inicio pero no de fin(reunion, extracurricular u otro)");
+                            if(in.getEstadoInicio()==true){
+                                System.out.println("estadoinicio=true");
+                                Asistencia asis= new Asistencia();
+                                asis.setIniciofin(in);
+                                asis.setEstado(true);
+                                asis.setTardanza(false);
+                                if(!this.existeAsistencia(in)){
+                                    asis.guardarAsistencia(asis);
+                                }
+                            }else{
+                                Calendar cel3=Calendar.getInstance();
+                                cel3.setTime(in.getFin());
+                                cel3.add(Calendar.MINUTE, -10);
+                                String s=new SimpleDateFormat("HH:mm").format(cal);
+                                SimpleDateFormat formateador=new SimpleDateFormat("HH:mm");
+                                Date a=formateador.parse(s);
+                                if(cel3.getTime().compareTo(a)<0){
+                                    Asistencia asis= new Asistencia();
+                                    asis.setIniciofin(in);
+                                    asis.setEstado(false);
+                                    asis.setTardanza(false);
+                                    if(!this.existeAsistencia(in)){
+                                        asis.guardarAsistencia(asis);
+                                    }
+                                    System.out.println("ausencia del personal");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }catch (Exception e){System.out.println(e.toString());}
+    }
+}
